129f2f2aea5f373d1ea4699f3af8a6ba5bd95557
Bùi Đức Huy <duchuy.124dk@gmail.com>
2020-09-17 17:52:58 +0700

    add server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const port = process.env.PORT || 3000
+const app = express()
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname + '/dist'))
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'))
+})
+
+app.listen(port)
+
+console.log(`app listen on port ${port}`)
